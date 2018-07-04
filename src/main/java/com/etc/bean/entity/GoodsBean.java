4dc3103ef494a8442252df8f8a1411e6b0a9166f
wjh <Administrator@PC201712221022>
2018-07-04 14:58:19 +0800

    商品和订单的bean

---
+++ src/main/java/com/etc/bean/entity/GoodsBean.java
@@ -0,0 +1,139 @@
+package com.etc.bean.entity;
+
+public class GoodsBean {
+	
+	private int goods_id; //商品编号 自增
+	private String goods_name; //商品名称
+	private double goods_minprice; //商品起拍价格
+	private double goods_currentprice; //商品当前竞拍价格
+	private double goods_getprice; //商品得标价格
+	private int goods_typeid; //商品类型编号
+	private String goods_desc; //商品描述
+	private int good_state; //商品竞拍情况（0未竞拍1正在竞拍2已出售 3流标）
+	private int user_id; //用户编号
+	private double goods_minpremium; //最低加价
+	private int goods_auctiontime; //商品拍卖时间（单位：天）
+	private double goods_margin; //商品竞拍保证金
+	private String goods_create; //新增时间
+	private String goods_modified; //更新时间
+	private String goodstype_desc; //类型描述
+	private String goodstype_create; //新增时间
+	private String goodstype_modified; //更新时间
+	
+	
+	@Override
+	public String toString() {
+		return "GoodsBean [goods_id=" + goods_id + ", goods_name=" + goods_name + ", goods_minprice=" + goods_minprice
+				+ ", goods_currentprice=" + goods_currentprice + ", goods_getprice=" + goods_getprice
+				+ ", goods_typeid=" + goods_typeid + ", goods_desc=" + goods_desc + ", good_state=" + good_state
+				+ ", user_id=" + user_id + ", goods_minpremium=" + goods_minpremium + ", goods_auctiontime="
+				+ goods_auctiontime + ", goods_margin=" + goods_margin + ", goods_create=" + goods_create
+				+ ", goods_modified=" + goods_modified + ", goodstype_desc=" + goodstype_desc + ", goodstype_create="
+				+ goodstype_create + ", goodstype_modified=" + goodstype_modified + "]";
+	}
+	public int getGoods_id() {
+		return goods_id;
+	}
+	public void setGoods_id(int goods_id) {
+		this.goods_id = goods_id;
+	}
+	public String getGoods_name() {
+		return goods_name;
+	}
+	public void setGoods_name(String goods_name) {
+		this.goods_name = goods_name;
+	}
+	public double getGoods_minprice() {
+		return goods_minprice;
+	}
+	public void setGoods_minprice(double goods_minprice) {
+		this.goods_minprice = goods_minprice;
+	}
+	public double getGoods_currentprice() {
+		return goods_currentprice;
+	}
+	public void setGoods_currentprice(double goods_currentprice) {
+		this.goods_currentprice = goods_currentprice;
+	}
+	public double getGoods_getprice() {
+		return goods_getprice;
+	}
+	public void setGoods_getprice(double goods_getprice) {
+		this.goods_getprice = goods_getprice;
+	}
+	public int getGoods_typeid() {
+		return goods_typeid;
+	}
+	public void setGoods_typeid(int goods_typeid) {
+		this.goods_typeid = goods_typeid;
+	}
+	public String getGoods_desc() {
+		return goods_desc;
+	}
+	public void setGoods_desc(String goods_desc) {
+		this.goods_desc = goods_desc;
+	}
+	public int getGood_state() {
+		return good_state;
+	}
+	public void setGood_state(int good_state) {
+		this.good_state = good_state;
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public double getGoods_minpremium() {
+		return goods_minpremium;
+	}
+	public void setGoods_minpremium(double goods_minpremium) {
+		this.goods_minpremium = goods_minpremium;
+	}
+	public int getGoods_auctiontime() {
+		return goods_auctiontime;
+	}
+	public void setGoods_auctiontime(int goods_auctiontime) {
+		this.goods_auctiontime = goods_auctiontime;
+	}
+	public double getGoods_margin() {
+		return goods_margin;
+	}
+	public void setGoods_margin(double goods_margin) {
+		this.goods_margin = goods_margin;
+	}
+	public String getGoods_create() {
+		return goods_create;
+	}
+	public void setGoods_create(String goods_create) {
+		this.goods_create = goods_create;
+	}
+	public String getGoods_modified() {
+		return goods_modified;
+	}
+	public void setGoods_modified(String goods_modified) {
+		this.goods_modified = goods_modified;
+	}
+	public String getGoodstype_desc() {
+		return goodstype_desc;
+	}
+	public void setGoodstype_desc(String goodstype_desc) {
+		this.goodstype_desc = goodstype_desc;
+	}
+	public String getGoodstype_create() {
+		return goodstype_create;
+	}
+	public void setGoodstype_create(String goodstype_create) {
+		this.goodstype_create = goodstype_create;
+	}
+	public String getGoodstype_modified() {
+		return goodstype_modified;
+	}
+	public void setGoodstype_modified(String goodstype_modified) {
+		this.goodstype_modified = goodstype_modified;
+	}
+	
+	
+
+}
---
+++ src/main/java/com/etc/bean/entity/OrderBean.java
@@ -0,0 +1,109 @@
+package com.etc.bean.entity;
+
+public class OrderBean {
+	
+	private int order_id; //订单编号 自增
+	private int user_id; //用户编号
+	private int goods_id; //商品编号
+	private String goods_name; //商品名称
+	private String goodstype_desc; //类型描述
+	private double goods_getprice; //商品得标价格
+	private String address_id; //送货地址编号
+	private String consignee_address; //收货人地址
+	private String senddate; //发货时间
+	private String payment; //付款方式
+	private double sendprice; //运费
+	private int order_state; //订单状态(1交易成功0交易关闭2未付款）
+	private String order_date;//订购日期
+	
+	
+	@Override
+	public String toString() {
+		return "OrderBean [order_id=" + order_id + ", user_id=" + user_id + ", goods_id=" + goods_id + ", goods_name="
+				+ goods_name + ", goodstype_desc=" + goodstype_desc + ", goods_getprice=" + goods_getprice
+				+ ", address_id=" + address_id + ", consignee_address=" + consignee_address + ", senddate=" + senddate
+				+ ", payment=" + payment + ", sendprice=" + sendprice + ", order_state=" + order_state + ", order_date="
+				+ order_date + "]";
+	}
+	public int getOrder_id() {
+		return order_id;
+	}
+	public void setOrder_id(int order_id) {
+		this.order_id = order_id;
+	}
+	public int getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(int user_id) {
+		this.user_id = user_id;
+	}
+	public int getGoods_id() {
+		return goods_id;
+	}
+	public void setGoods_id(int goods_id) {
+		this.goods_id = goods_id;
+	}
+	public String getGoods_name() {
+		return goods_name;
+	}
+	public void setGoods_name(String goods_name) {
+		this.goods_name = goods_name;
+	}
+	public String getGoodstype_desc() {
+		return goodstype_desc;
+	}
+	public void setGoodstype_desc(String goodstype_desc) {
+		this.goodstype_desc = goodstype_desc;
+	}
+	public double getGoods_getprice() {
+		return goods_getprice;
+	}
+	public void setGoods_getprice(double goods_getprice) {
+		this.goods_getprice = goods_getprice;
+	}
+	public String getAddress_id() {
+		return address_id;
+	}
+	public void setAddress_id(String address_id) {
+		this.address_id = address_id;
+	}
+	public String getConsignee_address() {
+		return consignee_address;
+	}
+	public void setConsignee_address(String consignee_address) {
+		this.consignee_address = consignee_address;
+	}
+	public String getSenddate() {
+		return senddate;
+	}
+	public void setSenddate(String senddate) {
+		this.senddate = senddate;
+	}
+	public String getPayment() {
+		return payment;
+	}
+	public void setPayment(String payment) {
+		this.payment = payment;
+	}
+	public double getSendprice() {
+		return sendprice;
+	}
+	public void setSendprice(double sendprice) {
+		this.sendprice = sendprice;
+	}
+	public int getOrder_state() {
+		return order_state;
+	}
+	public void setOrder_state(int order_state) {
+		this.order_state = order_state;
+	}
+	public String getOrder_date() {
+		return order_date;
+	}
+	public void setOrder_date(String order_date) {
+		this.order_date = order_date;
+	}
+	
+	
+
+}
